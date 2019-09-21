17c54ce3075961fe6e16d4592d782b3ce47f26ff
Diana <5275194+DianaNites@users.noreply.github.com>
2019-09-21 17:46:55 -0400

    WIP GPT header

--- src/gpt.rs
+++ src/gpt.rs
@@ -17,12 +17,59 @@ enum InnerError {
 
 type Result<T, E = GptError> = std::result::Result<T, E>;
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct GptHeader {
-    //
+    // Hard-coded to "EFI PART"
+    signature: String,
+
+    // Currently hard-coded to `1.0`/`0x00010000`, but may change?
+    revision: u32,
+
+    // Must be header_size >= 92 and header_size <= logical block size
+    header_size: u32,
+
+    // CRC32(bytes[0..header_size])
+    header_crc32: u32,
+
+    // Must be zero
+    _reserved: u32,
+
+    // The logical block address we reside in
+    this_lba: u64,
+
+    // The logical block address the backup header is in
+    alt_lba: u64,
+
+    // Where partitions can start
+    first_usable_lba: u64,
+
+    // Where partitions must end
+    last_usable_lba: u64,
+
+    // Disk GUID
+    disk_guid: u128,
+
+    // Where our partition array starts on disk.
+    partition_entries_lba: u64,
+
+    // Number of partitions
+    partition_len: u64,
+
+    // Size of each partition entry structure
+    // Must be 128 * 2^n, where n >= 0
+    partition_entry_size: u32,
+
+    // CRC32 of the partition array
+    partitions_crc32: u32,
 }
 
-#[derive(Debug)]
+impl GptHeader {
+    pub fn new() -> Self {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug, Default)]
 pub struct Gpt {
     mbr: ProtectiveMbr,
     header: GptHeader,
@@ -39,12 +86,16 @@ impl Gpt {
         RS: Read + Seek,
     {
         let mbr = ProtectiveMbr::from_reader(&mut source).context(MbrError)?;
-        let header = GptHeader {};
+        let header = GptHeader::new();
         Ok(Self {
             //
             mbr,
             header,
         })
+    }
+
+    pub fn from_bytes(_source: &[u8]) -> Result<Self> {
+        unimplemented!()
     }
 
     pub fn from_file() -> Self {
