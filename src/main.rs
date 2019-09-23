9c556ce830563b142156a81607b3891fcba9e76b
Diana <5275194+DianaNites@users.noreply.github.com>
2019-09-22 20:26:56 -0400

    cruft

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-#![allow(unused_imports, dead_code, unused_mut)]
 use parts::*;
-use std::io::Cursor;
 
 fn main() {
-    let mut buff = std::fs::File::open("scratch/test_parts").unwrap();
+    let buff = std::fs::File::open("scratch/test_parts").unwrap();
     let gpt = Gpt::from_reader(buff, 512);
     dbg!(&gpt);
     match gpt {
