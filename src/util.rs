a04f109e7ebc304a37bcf1b2b3a1c657412686e2
Diana <5275194+DianaNites@users.noreply.github.com>
2020-03-03 21:50:54 -0500

    parted test

--- src/util.rs
+++ src/util.rs
@@ -5,6 +5,9 @@ use std::{fs, io::prelude::*};
 /// Path to cfdisk test data
 pub static TEST_PARTS_CF: &str = "tests/data/test_parts_cf";
 
+/// Path to parted test data
+pub static TEST_PARTS: &str = "tests/data/test_parts";
+
 /// Test data block size
 pub const BLOCK_SIZE: BlockSize = BlockSize(512);
 
@@ -25,6 +28,14 @@ pub type Result<T = ()> = anyhow::Result<T>;
 pub fn data() -> Result<Vec<u8>> {
     let mut data = vec![0; TEN_MIB_BYTES];
     let mut file = fs::File::open(TEST_PARTS_CF)?;
+    file.read_exact(&mut data)?;
+    Ok(data)
+}
+
+/// Read test data from parted
+pub fn data_parted() -> Result<Vec<u8>> {
+    let mut data = vec![0; TEN_MIB_BYTES];
+    let mut file = fs::File::open(TEST_PARTS)?;
     file.read_exact(&mut data)?;
     Ok(data)
 }
