c227e7d859339354fd7c234a677278e5fa4246fd
Diana <5275194+DianaNites@users.noreply.github.com>
2020-06-08 11:39:13 -0400

    checks

--- src/gpt/partition.rs
+++ src/gpt/partition.rs
@@ -258,7 +258,17 @@ impl PartitionBuilder {
     /// New builder
     ///
     /// `uuid` is a unique identifer for the partition.
+    ///
+    /// # Panics
+    ///
+    /// - If `uuid` is nil.
     pub fn new(uuid: Uuid, gpt: &crate::Gpt) -> Self {
+        // Testing uses nil UUID's for convenience.
+        #[cfg(not(test))]
+        assert!(
+            !uuid.is_nil(),
+            "UUID cannot be nil. Partitions MUST be uniquely identifiable."
+        );
         Self {
             start: Default::default(),
             end: Default::default(),
